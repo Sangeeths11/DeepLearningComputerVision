25fb4bd37a1d0c310cae1c756f80ffeb0af83fcd
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-10-24 20:28:28 +0200

    GPU Server Sweep Config and Documentation

--- README.md
+++ README.md
@@ -2,3 +2,31 @@
 
 ## Beschreibung
 Dieses Repository enthält den Code für die Wartelinie-Erkennung auf SwissData Geo-Bildern mithilfe von Deep Learning für Computer Vision.
+
+## GPU Server
+
+Die aktuelle GPU Auslastung kann mittels `sudo nvtop` angezeigt werden. Die slurm Queue wird mittels `squeue` angezeigt und ein Job kann durch `scancel <JOBID>` gestoppt werden.
+
+Um einen Slurm Job zu starten wird der folgende Befehl verwendet.
+
+```shell
+salloc -p students --time=2:00:00 -G a100:1 --ntasks=32 --mem-per-cpu=7G
+```
+
+Ist der Job an der Reihe (siehe squeue) kann mittels dem nachfolgenden Befehl eine Interaktive Apptainer Shell verwendet werden.
+
+```shell
+apptainer shell --nv "${HOME}/build-apptainer/tensorflow-2.16.1-gpu.sif"
+```
+
+Nun werden im Apptainer die entsprechenden Packete installiert.
+
+```shell
+pip install -r "${HOME}/VisionTransformer/requirements.txt"
+```
+
+Schlussendlich wird der Code ausgeführt.
+
+```shell
+python3 "${HOME}/VisionTransformer/CNN.py"
+```
--- pythonScribt/CNN.py
+++ pythonScribt/CNN.py
@@ -1,33 +1,48 @@
 import os
-import numpy as np
+from pathlib import Path
+
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array # type: ignore
-from tensorflow.keras.models import Sequential # type: ignore
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization # type: ignore
-from tensorflow.keras.optimizers import Adam # type: ignore
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping # type: ignore
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
-import wandb
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau  # type: ignore
+from tensorflow.keras.layers import BatchNormalization  # type: ignore
+from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
+from tensorflow.keras.models import Sequential  # type: ignore
+from tensorflow.keras.optimizers import Adam  # type: ignore
+from tensorflow.keras.preprocessing.image import (  # type: ignore
+    ImageDataGenerator,
+    img_to_array,
+    load_img,
+)
 from wandb.integration.keras import WandbMetricsLogger
+
+import wandb
+
+matplotlib.use("Agg")
+
+# Path Configuration
+DATA_PATH = os.path.join(Path.home(), "VisionTransformer", "data")
 
 # Sweep configuration
 sweep_config = {
-    'name': 'CNN-Sweep',
-    'method': 'bayes',
-    'metric': {'name': 'val_accuracy', 'goal': 'maximize'},
-    'parameters': {
-        'learning_rate': {'min': 0.00001, 'max': 0.001},
-        'batch_size': {'values': [16, 32, 64]},
-        'dropout': {'min': 0.2, 'max': 0.5}
-    }
+    "name": "CNN-Sweep",
+    "method": "bayes",
+    "metric": {"name": "val_accuracy", "goal": "maximize"},
+    "parameters": {
+        "learning_rate": {"min": 0.00001, "max": 0.001},
+        "batch_size": {"values": [16, 32, 64]},
+        "dropout": {"min": 0.2, "max": 0.5},
+    },
 }
 
+
 class ImageClassifier:
-    def __init__(self, image_size=(250, 250), batch_size=32, project_name="VisionTransformer"):
+    def __init__(
+        self, image_size=(250, 250), batch_size=32, project_name="VisionTransformer"
+    ):
         self.image_size = image_size
         self.batch_size = batch_size
         self.model = None
@@ -47,57 +62,93 @@ class ImageClassifier:
         return np.array(images), np.array(labels)
 
     def prepare_data(self, path_with_sign, path_without_sign):
-        images_with_sign, labels_with_sign = self.load_images_from_folder(path_with_sign, 1)
-        images_without_sign, labels_without_sign = self.load_images_from_folder(path_without_sign, 0)
-        
-        all_images = np.concatenate((images_with_sign, images_without_sign), axis=0) / 255.0
+        images_with_sign, labels_with_sign = self.load_images_from_folder(
+            path_with_sign, 1
+        )
+        images_without_sign, labels_without_sign = self.load_images_from_folder(
+            path_without_sign, 0
+        )
+
+        all_images = (
+            np.concatenate((images_with_sign, images_without_sign), axis=0) / 255.0
+        )
         all_labels = np.concatenate((labels_with_sign, labels_without_sign), axis=0)
-        
-        train_images, test_images, train_labels, test_labels = train_test_split(all_images, all_labels, test_size=0.2, random_state=42)
-        train_images, validation_images, train_labels, validation_labels = train_test_split(train_images, train_labels, test_size=0.2, random_state=42)
-        
-        return train_images, test_images, validation_images, train_labels, test_labels, validation_labels
+
+        train_images, test_images, train_labels, test_labels = train_test_split(
+            all_images, all_labels, test_size=0.2, random_state=42
+        )
+        train_images, validation_images, train_labels, validation_labels = (
+            train_test_split(train_images, train_labels, test_size=0.2, random_state=42)
+        )
+
+        return (
+            train_images,
+            test_images,
+            validation_images,
+            train_labels,
+            test_labels,
+            validation_labels,
+        )
 
     def build_model(self, config):
-        model = Sequential([
-            Conv2D(32, (3, 3), activation='relu', input_shape=(self.image_size[0], self.image_size[1], 3)),
-            BatchNormalization(),
-            MaxPooling2D(2, 2),
-            
-            Conv2D(64, (3, 3), activation='relu'),
-            BatchNormalization(),
-            MaxPooling2D(2, 2),
-            
-            Conv2D(128, (3, 3), activation='relu'),
-            BatchNormalization(),
-            MaxPooling2D(2, 2),
-            
-            Conv2D(256, (3, 3), activation='relu'),
-            BatchNormalization(),
-            MaxPooling2D(2, 2),
-            
-            Flatten(),
-            Dense(128, activation='relu'),
-            Dropout(config.dropout),
-            Dense(1, activation='sigmoid')
-        ])
-        model.compile(optimizer=Adam(learning_rate=config.learning_rate), loss='binary_crossentropy', metrics=['accuracy'])
+        model = Sequential(
+            [
+                Conv2D(
+                    32,
+                    (3, 3),
+                    activation="relu",
+                    input_shape=(self.image_size[0], self.image_size[1], 3),
+                ),
+                BatchNormalization(),
+                MaxPooling2D(2, 2),
+                Conv2D(64, (3, 3), activation="relu"),
+                BatchNormalization(),
+                MaxPooling2D(2, 2),
+                Conv2D(128, (3, 3), activation="relu"),
+                BatchNormalization(),
+                MaxPooling2D(2, 2),
+                Conv2D(256, (3, 3), activation="relu"),
+                BatchNormalization(),
+                MaxPooling2D(2, 2),
+                Flatten(),
+                Dense(128, activation="relu"),
+                Dropout(config.dropout),
+                Dense(1, activation="sigmoid"),
+            ]
+        )
+        model.compile(
+            optimizer=Adam(learning_rate=config.learning_rate),
+            loss="binary_crossentropy",
+            metrics=["accuracy"],
+        )
         self.model = model
 
     def train(self, train_images, train_labels, validation_images, validation_labels):
         train_datagen = ImageDataGenerator(
-            rotation_range=30, width_shift_range=0.2, height_shift_range=0.2,
-            shear_range=0.2, zoom_range=0.2, horizontal_flip=True,
+            rotation_range=30,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
             brightness_range=[0.8, 1.2],
-            fill_mode='nearest'
+            fill_mode="nearest",
         )
         validation_datagen = ImageDataGenerator()
 
-        train_generator = train_datagen.flow(train_images, train_labels, batch_size=self.batch_size)
-        validation_generator = validation_datagen.flow(validation_images, validation_labels, batch_size=self.batch_size)
+        train_generator = train_datagen.flow(
+            train_images, train_labels, batch_size=self.batch_size
+        )
+        validation_generator = validation_datagen.flow(
+            validation_images, validation_labels, batch_size=self.batch_size
+        )
 
-        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=0.00001)
-        early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+        reduce_lr = ReduceLROnPlateau(
+            monitor="val_loss", factor=0.2, patience=3, min_lr=0.00001
+        )
+        early_stopping = EarlyStopping(
+            monitor="val_loss", patience=5, restore_best_weights=True
+        )
 
         history = self.model.fit(
             train_generator,
@@ -105,35 +156,35 @@ class ImageClassifier:
             epochs=5,
             validation_data=validation_generator,
             validation_steps=len(validation_images) // self.batch_size,
-            callbacks=[WandbMetricsLogger(), reduce_lr, early_stopping]
+            callbacks=[WandbMetricsLogger(), reduce_lr, early_stopping],
         )
         return history
 
     def evaluate(self, test_images, test_labels):
         test_loss, test_acc = self.model.evaluate(test_images, test_labels)
-        wandb.log({'test_loss': test_loss, 'test_acc': test_acc})
+        wandb.log({"test_loss": test_loss, "test_acc": test_acc})
         return test_loss, test_acc
 
     def plot_training(self, history):
         plt.figure(figsize=(12, 5))
 
         plt.subplot(1, 2, 1)
-        plt.plot(history.history['accuracy'])
-        plt.plot(history.history['val_accuracy'])
-        plt.title('Model Accuracy')
-        plt.ylabel('Accuracy')
-        plt.xlabel('Epoch')
-        plt.legend(['Train', 'Validation'], loc='upper left')
+        plt.plot(history.history["accuracy"])
+        plt.plot(history.history["val_accuracy"])
+        plt.title("Model Accuracy")
+        plt.ylabel("Accuracy")
+        plt.xlabel("Epoch")
+        plt.legend(["Train", "Validation"], loc="upper left")
 
         plt.subplot(1, 2, 2)
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
-        plt.title('Model Loss')
-        plt.ylabel('Loss')
-        plt.xlabel('Epoch')
-        plt.legend(['Train', 'Validation'], loc='upper left')
+        plt.plot(history.history["loss"])
+        plt.plot(history.history["val_loss"])
+        plt.title("Model Loss")
+        plt.ylabel("Loss")
+        plt.xlabel("Epoch")
+        plt.legend(["Train", "Validation"], loc="upper left")
 
-        plt.suptitle('Model Training - Basic CNN')
+        plt.suptitle("Model Training - Basic CNN")
         plt.tight_layout()
         plt.savefig("training_plot.png")  # Save the plot instead of showing it
         wandb.log({"training_plot": wandb.Image("training_plot.png")})
@@ -142,49 +193,70 @@ class ImageClassifier:
     def predict_and_report(self, test_images, test_labels):
         predictions = (self.model.predict(test_images) > 0.5).astype(int)
         for i in range(len(test_images)):
-            wandb.log({
-                "image": [wandb.Image(test_images[i], caption=f"Prediction: {predictions[i]}, True: {test_labels[i]}")],
-                "prediction": predictions[i],
-                "true_label": test_labels[i]
-            })
+            wandb.log(
+                {
+                    "image": [
+                        wandb.Image(
+                            test_images[i],
+                            caption=f"Prediction: {predictions[i]}, True: {test_labels[i]}",
+                        )
+                    ],
+                    "prediction": predictions[i],
+                    "true_label": test_labels[i],
+                }
+            )
 
         # Log confusion matrix
         cm = confusion_matrix(test_labels, predictions)
-        sns.heatmap(cm, annot=True, cmap='Blues', fmt='d', xticklabels=['Wartelinie', 'keine Wartelinie'], yticklabels=['Wartelinie', 'keine Wartelinie'])
-        plt.xlabel('Predicted Labels')
-        plt.ylabel('True Labels')
-        plt.title('Confusion Matrix - Basic CNN Model')
+        sns.heatmap(
+            cm,
+            annot=True,
+            cmap="Blues",
+            fmt="d",
+            xticklabels=["Wartelinie", "keine Wartelinie"],
+            yticklabels=["Wartelinie", "keine Wartelinie"],
+        )
+        plt.xlabel("Predicted Labels")
+        plt.ylabel("True Labels")
+        plt.title("Confusion Matrix - Basic CNN Model")
         plt.savefig("confusion_matrix.png")
         wandb.log({"confusion_matrix": wandb.Image("confusion_matrix.png")})
         plt.close()
 
     def save_model(self):
-        model_artifact = wandb.Artifact('cnn-model', type='model')
-        self.model.save('cnn_model.h5')
-        model_artifact.add_file('cnn_model.h5')
+        model_artifact = wandb.Artifact("cnn-model", type="model")
+        self.model.save("cnn_model.h5")
+        model_artifact.add_file("cnn_model.h5")
         wandb.log_artifact(model_artifact)
 
 
 if __name__ == "__main__":
-    sweep_id = wandb.sweep(sweep_config, project='VisionTransformer')
+    sweep_id = wandb.sweep(sweep_config, project="VisionTransformer")
 
     def train_sweep():
         with wandb.init() as run:
             config = wandb.config
 
             run.name = f"lr_{config.learning_rate}_bs_{config.batch_size}_do_{config.dropout:.2f}"
-            
-            classifier = ImageClassifier(batch_size=config.batch_size)
-            
-            path_with_sign = 'data/y'
-            path_without_sign = 'data/n'
 
-            train_images, test_images, validation_images, train_labels, test_labels, validation_labels = classifier.prepare_data(
-                path_with_sign, path_without_sign
-            )
+            classifier = ImageClassifier(batch_size=config.batch_size)
+
+            path_with_sign = os.path.join(DATA_PATH, "y")
+            path_without_sign = os.path.join(DATA_PATH, "n")
+
+            (
+                train_images,
+                test_images,
+                validation_images,
+                train_labels,
+                test_labels,
+                validation_labels,
+            ) = classifier.prepare_data(path_with_sign, path_without_sign)
 
             classifier.build_model(config)
-            history = classifier.train(train_images, train_labels, validation_images, validation_labels)
+            history = classifier.train(
+                train_images, train_labels, validation_images, validation_labels
+            )
 
             classifier.evaluate(test_images, test_labels)
 
@@ -196,4 +268,3 @@ if __name__ == "__main__":
 
     wandb.agent(sweep_id, train_sweep)
     wandb.finish()
-
