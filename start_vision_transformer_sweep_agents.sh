410c2526c2d58153fededa20260564c8b18b6fe6
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-10-31 21:23:29 +0100

    utilies both gpus as much as feasible

--- start_vision_transformer_sweep_agents.sh
+++ start_vision_transformer_sweep_agents.sh
@@ -3,4 +3,8 @@
 SWEEP_ID="$1"
 
 CUDA_VISIBLE_DEVICES=0 python3 pythonScript/VisionTransformer.py $SWEEP_ID &
+CUDA_VISIBLE_DEVICES=0 python3 pythonScript/VisionTransformer.py $SWEEP_ID &
+CUDA_VISIBLE_DEVICES=0 python3 pythonScript/VisionTransformer.py $SWEEP_ID &
+CUDA_VISIBLE_DEVICES=1 python3 pythonScript/VisionTransformer.py $SWEEP_ID &
+CUDA_VISIBLE_DEVICES=1 python3 pythonScript/VisionTransformer.py $SWEEP_ID &
 CUDA_VISIBLE_DEVICES=1 python3 pythonScript/VisionTransformer.py $SWEEP_ID &
